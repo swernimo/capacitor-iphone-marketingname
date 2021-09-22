365571c8733c8bd8292f236668e3639332d543f3
Sean Wernimont <swernimo@users.noreply.github.com>
2021-09-22 10:17:45 -0600

    added to definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,9 @@
+declare module "@capacitor/core" {
+  interface PluginRegistry {
+    iPhoneMarketingName: iPhoneMarketingNamePlugin;
+  }
+}
+
 export interface iPhoneMarketingNamePlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
   getMarketingName(): Promise<{ marketingName: string }>;
